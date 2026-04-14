d58580f3abcdd272fc764f389a86f26ecdbd2e99
sherzod <suvonov2310@gmail.com>
2026-04-14 20:41:14 +0500

    Dizayn, MB kursi va Skoring testi qo'shildi

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,21 @@
+import requests
 from django.shortcuts import render
 from .models import Bank
 
 def kredit_page(request):
     banklar = Bank.objects.all()
-    return render(request, 'kredit.html', {'banklar': banklar})
+    
+    # Markaziy Bank API'dan kursni olish
+    usd_rate = 12850.0  # Default qiymat
+    try:
+        response = requests.get('https://cbu.uz/uz/arkhiv-kursov-valyut/json/USD/')
+        data = response.json()
+        if data:
+            usd_rate = float(data[0]['Rate'])
+    except:
+        pass
+
+    return render(request, 'kredit.html', {
+        'banklar': banklar,
+        'usd_rate': usd_rate,
+    })
